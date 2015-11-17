e95a2e077fd20bfefe7570f01ec330d4e9bd58ef
Anmol Raj Pant <anmolrajpant@hotmail.com>
2015-11-17 11:51:55 +0545

    layout changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,21 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipes = Recipe.find(parmas[:id])
   end
 
   def new
+    @recipes = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_parmas)
+    if @recipe.save
+      redirect_to recipes_path
+    end
   end
 
   def edit
@@ -21,5 +28,13 @@ class RecipesController < ApplicationController
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      redirect_to recipes_path
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title,:time,:ingredient,:producer)  
   end
 end
