e6d9402cd04e84cd343608ff3e69a17cbb12e850
Anmol Raj Pant <anmolrajpant@hotmail.com>
2015-11-17 18:18:31 +0545

    initial basic working layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,25 +1,45 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    end
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    end
   end
 
   def delete
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      redirect_to recipes_path
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :items, :process)    
   end
 end
