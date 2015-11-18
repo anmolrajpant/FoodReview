4c25a7c47053d7d25ada5992f66f280e30bcc8ad
Anmol Raj Pant <anmolrajpant@hotmail.com>
2015-11-18 19:03:07 +0545

    mail added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117144501) do
+ActiveRecord::Schema.define(version: 20151118092947) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20151117144501) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "recipe_id"
   end
 
   create_table "users", force: :cascade do |t|
